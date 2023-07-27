995625e134a68fdc02734a99096d5ff623aad433
ruby45dev <60508600+ruby45dev@users.noreply.github.com>
2023-07-27 15:04:49 +0300

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,28 @@
+puts 'Введіть логін до FTP'
+user_login = gets.chomp
+
+puts 'Введіть пароль до FTP'
+user_password = gets.chomp
+
+path_error_log = " " #вказую шлях до файлу на ПК
+
+if File.exist?(path_error_log)   
+
+    file_log = File.open(path_error_log, "a+")
+    file_log_lines = file_log.readlines
+    anny = []
+    not_anny = []
+    new =
+        file_log_lines.each do |item_tmp|
+            if item_tmp.include?('Warning')
+                anny << item_tmp
+            else
+                not_anny << item_tmp
+            end
+        end
+
+    file_new = File.open("#{__dir__}/sandbox1.txt", "a")
+    file_new.puts anny
+else
+    puts "У цій деректорії файл відсутній"
+end
