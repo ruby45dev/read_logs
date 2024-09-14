b4f0da8874a8f20cd5f59c3e9e56f7b3e29910db
ruby45 <discuss.ruby@gmail.com>
2024-09-14 17:38:58 +0300

    add system scp

--- main.rb
+++ main.rb
@@ -1,22 +1,34 @@
-path_error_log = " " #вказую шлях до файлу
+user = "" #ім'я користувача сервера
+host = "" #ip адреса або домен
+remote_file = "" #шлях до віддаленого файлу
+local_destination = "" # шлях до каталогу для розташування скачаного файлу
 
-if File.exist?(path_error_log)   
+result = system("scp #{user}@#{host}:#{remote_file} #{local_destination}")
+
+if result
+  puts "Файл успішно отримано"
+else
+  puts "Помилка під час отримання файлу"
+end
+
+path_error_log = "" #вказую шлях до файлу
+
+if File.exist?(path_error_log)
 
     file_log = File.open(path_error_log, "a+")
     file_log_lines = file_log.readlines
     errors = []
     infos = []
     others = []
-    new =
-        file_log_lines.each do |item_tmp|
-            if item_tmp.include?('ERROR:')
-                errors << item_tmp            
-            elsif item_tmp.include?('INFO:')
-                infos << item_tmp
-            else
-                others << item_tmp
-            end
+    file_log_lines.each do |item_tmp|
+        if item_tmp.include?('ERROR:')
+            errors << item_tmp
+        elsif item_tmp.include?('INFO:')
+            infos << item_tmp
+        else
+            others << item_tmp
         end
+    end
 
     file_errors = File.open("#{__dir__}/errors.txt", "a")
     file_errors.puts errors
